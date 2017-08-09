0580e8e36a12874f99a9c9b82dadfcdc3c67b56e
Hari <h_narayana@yahoo.com>
2017-08-09 17:44:51 +0530

    Initial commit
    Change-Id: I7845ddc701ed3147f3a0ae52594271cabe293e09
    Signed-off-by: Hari <h_narayana@yahoo.com>

---
+++ test/src/test/HelloIndia.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package test;
+
+ * @author User
+ *
+ */
+public class HelloIndia {
+public static void main(String[] args) 
+	
+	{
+		System.out.println("Hello This is my country India!");
+	}
+}
---
+++ test/src/test/unittest.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class unittest {
+
+	@Test
+	public void binaryconvertor () {
+		System.out.println("Hello This is my test!");
+			}
+	}
